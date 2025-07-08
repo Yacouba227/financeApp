f6eaa1c94eefa1e950fd7bbfa982fae3e43260a7
Yacouba227 <halidouyacouba999@gmail.com>
2025-07-08 01:06:04 +0100

    Statistics pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:financeapp/home.dart';
+import 'package:financeapp/statistics.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,9 +12,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Home(),
-    );
+    return MaterialApp(debugShowCheckedModeBanner: false, home: Statistics());
   }
 }
--- lib/statistics.dart
+++ lib/statistics.dart
@@ -1,3 +1,6 @@
+import 'dart:ffi';
+
+import 'package:financeapp/widgets/chart.dart';
 import 'package:flutter/material.dart';
 
 class Statistics extends StatefulWidget {
@@ -8,10 +11,112 @@ class Statistics extends StatefulWidget {
 }
 
 class _StatisticsState extends State<Statistics> {
+  List day = ['Day', 'Week', 'Month', 'Year'];
+  int selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       //42:37
+      body: SafeArea(
+        child: CustomScrollView(
+          slivers: [
+            SliverToBoxAdapter(
+              child: Column(
+                children: [
+                  Text(
+                    'Statistics',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 15),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        ...List.generate(4, (index) {
+                          return GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                selectedIndex == index;
+                              });
+                            },
+                            child: Container(
+                              height: 40,
+                              width: 80,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                color: selectedIndex == index
+                                    ? Color.fromARGB(255, 47, 125, 121)
+                                    : Colors.white,
+                              ),
+                              alignment: Alignment.center,
+                              child: Text(
+                                day[index],
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  color: selectedIndex == index
+                                      ? Colors.white
+                                      : Colors.black,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                            ),
+                          );
+                        }),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 15),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Container(
+                          width: 120,
+                          height: 40,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              Text(
+                                'Expenses',
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Icon(
+                                Icons.arrow_downward_sharp,
+                                color: Colors.grey,
+                              ),
+                            ],
+                          ),
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.grey, width: 2),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Chart(),
+                  SizedBox(height: 20),
+                  Row(
+                    //57:33
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
